55059d952137f6754001b1a75404028bb13929fc
Prashant Singh <psingh@orkutics.com>
2018-01-12 01:14:44 +0000

    Eureka integration to connect with Eureka Server

--- src/main/java/co/uk/app/commerce/users/application/UsersApplication.java
+++ src/main/java/co/uk/app/commerce/users/application/UsersApplication.java
@@ -3,6 +3,7 @@ package co.uk.app.commerce.users.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan("co.uk.app.commerce.users")
 @EnableJpaRepositories("co.uk.app.commerce.users.*")
